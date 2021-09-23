81996ccec62004bc2896e4225e5f7cbcb3add1c6
synecdochenoire <synecdochenoire@gmail.com>
2021-09-23 19:32:10 +0300

    Create Annotation and implement showing places

--- Placeeo/Controller/MainViewController.swift
+++ Placeeo/Controller/MainViewController.swift
@@ -9,28 +9,29 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MainViewController: MapViewController{
-
-  let mapVC = MapViewController()
+class MainViewController: UIViewController, MKMapViewDelegate{
+    
+    private let mapView: MapView = {
+        MapView.loadFromNib() as! MapView
+    }()
+    
+    let places: [Place] = [
+        Place(name: "Castle Uzhgorod", lat: 48.62216095313416, long: 22.308029690972464),
+        Place(name: "Church", lat: 48.623380761219515, long: 22.302193204377218)
+    ]
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        view.addSubview(mapView)
         
-          let placeOne = Place(name: "Castle Uzhgorod", lat: 48.62216095313416, long: 22.308029690972464)
-          let placeTwo = Place(name: "Church", lat: 48.623380761219515, long: 22.302193204377218)
-          
-          
-          let places = [placeOne, placeTwo]
-          
-   
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        
-      let mapViewController = MapViewController()
-   
+        mapView.show(places: places)
     }
-    
-  
-
-    
-    
 }
+
