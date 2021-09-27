a3bdb7bf82b3a02f7094303296e73a2c18649e3d
synecdochenoire <87466650+synecdochenoire@users.noreply.github.com>
2021-09-27 15:21:10 +0300

    LA-23 Map Screen  (#1)
    * Optimize to MVC pattern
    
    * Fix File structure
    
    * Create CLLocationManager, Delegate and Show current user Location
    
    * Create an Anotattion on map
    
    * Recreation and optimizing  (#3)
    
    * Recreated from scratch MapViewController with more safety and clearity in code
    
    * Create Annotation and implement showing places
    
    Co-authored-by: Nazarii Melnyk <nazarij.melnyk@gmail.com>

---
+++ Placeeo/Controller/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Placeeo
+//
+//  Created by Pasha Berger on 13.09.2021.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MainViewController: UIViewController, MKMapViewDelegate{
+    
+    private let mapView: MapView = {
+        MapView.loadFromNib() as! MapView
+    }()
+    
+    let places: [Place] = [
+        Place(name: "Castle Uzhgorod", lat: 48.62216095313416, long: 22.308029690972464),
+        Place(name: "Church", lat: 48.623380761219515, long: 22.302193204377218)
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(mapView)
+        
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        mapView.show(places: places)
+    }
+}
+
