0293669848b3596594619449a99a85634e8ca050
synecdochenoire <synecdochenoire@gmail.com>
2021-10-28 14:05:06 +0300

    Networking settings and implementetion of API

--- Placeeo/Controller/MainViewController.swift
+++ Placeeo/Controller/MainViewController.swift
@@ -9,40 +9,35 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MainViewController: UIViewController, MKMapViewDelegate{
+class MainViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     private let mapView: MapView = {
         MapView.loadFromNib() as! MapView
     }()
     
-    
-    let places: [Place] = [
-        Place(name: "Castle Uzhgorod", lat: 48.62216095313416, long: 22.308029690972464),
-        Place(name: "Church", lat: 48.623380761219515, long: 22.302193204377218)
-    ]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         title = "Home"
-        mapView.show(places: places)
-        LocationManager.shared.getUserLocation{ [weak self] location in
+        LocationManager.shared.getUserLocation { [weak self] location in
             DispatchQueue.main.async {
-                guard let strongSelf = self else {
-                    return
+                guard let strongSelf = self else { return }
+                GooglePlacesManager.shared.getPlaces(around: location.coordinate) { places in
+                    strongSelf.mapView.show(places: places)
                 }
-                strongSelf.mapView.addPin(with: location)
-                LocationManager.shared.resolveLocationName(with: location, completion: { [weak self] locationName in self?.title = locationName})
             }
         }
-     
+        mapView.centerViewOnUserLocation()
+        mapView.mapView.showsUserLocation = true
+        
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        mapView.frame = view.bounds
-    }
-
     
 }
+
 
--- Placeeo/Resources/AppDelegate.swift
+++ Placeeo/Resources/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
+        GMSPlacesClient.provideAPIKey("AIzaSyBLVcVo5N4mcs1PASpBMYzgpKuff_gkz8A")
         return true
     }
 
