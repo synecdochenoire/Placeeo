1d395b38804b6f638aa35d0892d504b2d4ab1441
synecdochenoire <87466650+synecdochenoire@users.noreply.github.com>
2021-10-28 14:31:05 +0300

    LA-30: Implement Google API`s (#7)
    * LA-30: Implement Google API`s
    
    * Networking settings and implementation of API

--- Placeeo/Controller/MainViewController.swift
+++ Placeeo/Controller/MainViewController.swift
@@ -9,29 +9,35 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MainViewController: UIViewController, MKMapViewDelegate{
+class MainViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     private let mapView: MapView = {
         MapView.loadFromNib() as! MapView
     }()
     
-    let places: [Place] = [
-        Place(name: "Castle Uzhgorod", lat: 48.62216095313416, long: 22.308029690972464),
-        Place(name: "Church", lat: 48.623380761219515, long: 22.302193204377218)
-    ]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(mapView)
-        
         NSLayoutConstraint.activate([
             mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            mapView.trailingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mapView.topAnchor.constraint(equalTo: view.topAnchor),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        title = "Home"
+        LocationManager.shared.getUserLocation { [weak self] location in
+            DispatchQueue.main.async {
+                guard let strongSelf = self else { return }
+                GooglePlacesManager.shared.getPlaces(around: location.coordinate) { places in
+                    strongSelf.mapView.show(places: places)
+                }
+            }
+        }
+        mapView.centerViewOnUserLocation()
+        mapView.mapView.showsUserLocation = true
         
-        mapView.show(places: places)
     }
+    
 }
+
 
---
+++ Placeeo/Model/GooglePlacesManager.swift
@@ -0,0 +1,64 @@
+//
+//  GooglePlacesManager.swift
+//  Placeeo
+//
+//  Created by Pasha Berger on 11.10.2021.
+//
+
+import Foundation
+import GooglePlaces
+import CoreLocation
+final class GooglePlacesManager {
+    
+    let apiKey = "AIzaSyBLVcVo5N4mcs1PASpBMYzgpKuff_gkz8A"
+    
+    static let shared = GooglePlacesManager()
+    
+    private let client = GMSPlacesClient.shared()
+    
+    private init (){}
+    
+    enum PlacesError: Error{
+        case failedToFind
+        case failedToGetCoords
+    }
+    
+    func getPlaces(around location: CLLocationCoordinate2D, completion: @escaping ([Place]) -> Void) {
+        guard let url = URL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(location.latitude),\(location.longitude)&radius=50000&type=restaurant&key=\(apiKey)") else {
+            completion([])
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {return}
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+                    guard let dictionary = json as? [String: Any],
+                          let results = dictionary["results"] as? [Any]
+                    else { return }
+                let places: [Place] = results.compactMap { result in
+                    let place = result as? [String: Any]
+                   
+                    let geometry = place?["geometry"] as? [String: Any]
+                    let coordinates = geometry?["location"] as? [String: Double]
+                   guard let lat = coordinates?["lat"],
+                         let name = place?["name"] as? String,
+                         let lng = coordinates?["lng"]
+                   else {return nil}
+                    let placeCoordinates = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+                  
+                    return Place(name: name, coordinates: placeCoordinates)
+                   
+                }
+                completion(places)
+            } catch {
+                
+            }
+        }
+        .resume()
+        return
+    }
+    
+}
+
+
